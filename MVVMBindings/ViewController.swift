5507749ed0777c899ccd95c90377acbc2369323d
HunterAmani <amanikhunter@gmail.com>
2021-05-01 18:45:39 -0400

    Initial Commit

---
+++ MVVMBindings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMBindings
+//
+//  Created by Amani Hunter on 5/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
